16411d3555d5f944bcbf8902b0bb45cf416daccb
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-02-22 20:26:03 +0100

    add spawn example

---
+++ examples/spawn.rs
@@ -0,0 +1,69 @@
+use bevy::prelude::*;
+use bevy_editor_pls::{EditorPlugin, EditorSettings};
+use bevy_mod_picking::{PickableBundle, PickingCameraBundle};
+
+pub enum Event {
+    Cube,
+    Sphere,
+}
+
+fn editor_settings() -> EditorSettings {
+    let mut settings = EditorSettings::default();
+    settings.add_event("Spawn Cube", || Event::Cube);
+    settings.add_event("Spawn Sphere", || Event::Sphere);
+    settings
+}
+
+fn main() {
+    App::build()
+        .insert_resource(Msaa { samples: 4 })
+        .insert_resource(editor_settings())
+        .add_plugins(DefaultPlugins)
+        .add_plugin(EditorPlugin)
+        .add_event::<Event>()
+        .add_startup_system(setup.system())
+        .add_system(spawn_events.system())
+        .run();
+}
+
+fn spawn_events(
+    commands: &mut Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut events: EventReader<Event>,
+) {
+    for event in events.iter() {
+        let mesh = match event {
+            Event::Cube => Mesh::from(shape::Cube { size: 1.0 }),
+            Event::Sphere => Mesh::from(shape::Icosphere {
+                radius: 1.0,
+                subdivisions: 16,
+            }),
+        };
+
+        commands
+            .spawn(PbrBundle {
+                mesh: meshes.add(mesh),
+                material: materials.add(Color::WHITE.into()),
+                ..Default::default()
+            })
+            .with_bundle(PickableBundle::default());
+    }
+}
+
+pub fn setup(
+    commands: &mut Commands,
+    mut _meshes: ResMut<Assets<Mesh>>,
+    mut _materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        .spawn(LightBundle {
+            transform: Transform::from_xyz(4.0, 8.0, 4.0),
+            ..Default::default()
+        })
+        .spawn(PerspectiveCameraBundle {
+            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::default(), Vec3::unit_y()),
+            ..Default::default()
+        })
+        .with_bundle(PickingCameraBundle::default());
+}
