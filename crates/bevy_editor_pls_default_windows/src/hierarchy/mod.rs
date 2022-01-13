5039743fcd5f06697fa4a12919fe5fe037b65213
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-01-13 16:25:23 +0100

    always show selected entity in hierarchy

--- crates/bevy_editor_pls_default_windows/src/hierarchy/mod.rs
+++ crates/bevy_editor_pls_default_windows/src/hierarchy/mod.rs
@@ -1,7 +1,7 @@
 pub mod picking;
 
-use bevy::ecs::system::QuerySingleError;
 use bevy::prelude::*;
+use bevy::{ecs::system::QuerySingleError, utils::HashSet};
 use bevy_inspector_egui::egui::{self, CollapsingHeader, RichText};
 
 use bevy_editor_pls_core::{
@@ -70,12 +70,19 @@ struct Hierarchy<'a> {
 impl<'a> Hierarchy<'a> {
     fn show(&mut self, ui: &mut egui::Ui) {
         let mut root_query = self.world.query_filtered::<Entity, Without<Parent>>();
+
+        let parents: HashSet<Entity> = std::iter::successors(self.state.selected, |&entity| {
+            self.world.get::<Parent>(entity).map(|parent| parent.0)
+        })
+        .skip(1)
+        .collect();
+
         let entities: Vec<_> = root_query.iter(self.world).collect();
         for entity in entities {
-            self.entity_ui(entity, ui);
+            self.entity_ui(entity, ui, &parents);
         }
     }
-    fn entity_ui(&mut self, entity: Entity, ui: &mut egui::Ui) {
+    fn entity_ui(&mut self, entity: Entity, ui: &mut egui::Ui, always_open: &HashSet<Entity>) {
         let active = self.state.selected == Some(entity);
 
         let entity_name = bevy_inspector_egui::world_inspector::entity_name(self.world, entity);
@@ -90,17 +97,25 @@ impl<'a> Hierarchy<'a> {
             .get::<Children>(entity)
             .map_or(false, |children| children.len() > 0);
 
+        let open = if !has_children {
+            Some(false)
+        } else if always_open.contains(&entity) {
+            Some(true)
+        } else {
+            None
+        };
+
         let response = CollapsingHeader::new(text)
             .selectable(true)
             .selected(selected)
-            .open(if !has_children { Some(false) } else { None })
+            .open(open)
             .show(ui, |ui| {
                 let children = self.world.get::<Children>(entity);
                 if let Some(children) = children {
                     let children = children.clone();
                     ui.label("Children");
                     for &child in children.iter() {
-                        self.entity_ui(child, ui);
+                        self.entity_ui(child, ui, always_open);
                     }
                 } else {
                     ui.label("No children");
