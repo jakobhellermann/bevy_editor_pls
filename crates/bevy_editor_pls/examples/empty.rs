935c9ff683692da20a024a3700397bd8116ebc07
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-02-12 17:23:26 +0100

    make empty example actually empty

--- crates/bevy_editor_pls/examples/empty.rs
+++ crates/bevy_editor_pls/examples/empty.rs
@@ -1,20 +1,9 @@
 use bevy::prelude::*;
 use bevy_editor_pls::prelude::*;
-use bevy_editor_pls_default_windows::hierarchy::picking::EditorRayCastSource;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(EditorPlugin)
-        .add_startup_system(setup)
         .run();
-}
-
-fn setup(mut commands: Commands) {
-    commands
-        .spawn_bundle(PerspectiveCameraBundle {
-            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-            ..Default::default()
-        })
-        .insert(EditorRayCastSource::new());
 }
