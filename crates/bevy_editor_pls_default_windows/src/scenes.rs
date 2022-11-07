f767b2d2c1952a8753b2dbcfc53669dc25b3f056
PhaestusFox <phaestusfox@gmail.com>
2022-11-07 15:18:11 +0800

    added skip in scene
    added a NotInScene component allowing entity's to be skipped when saving a scene.

--- crates/bevy_editor_pls_default_windows/src/scenes.rs
+++ crates/bevy_editor_pls_default_windows/src/scenes.rs
@@ -4,6 +4,9 @@ use bevy_inspector_egui::egui::{self, RichText};
 
 const DEFAULT_FILENAME: &str = "scene.scn.ron";
 
+#[derive(Default, Component)]
+pub struct NotInScene;
+
 #[derive(Default)]
 pub struct SceneWindowState {
     filename: String,
@@ -37,7 +40,9 @@ impl EditorWindow for SceneWindow {
                 } else {
                     &state.filename
                 };
-                state.scene_save_result = Some(save_world(world, filename));
+                let mut query = world.query_filtered::<Entity, Without<NotInScene>>();
+                let entitys = query.iter(world).collect();
+                state.scene_save_result = Some(save_world(world, filename, entitys));
             }
         });
 
@@ -54,12 +59,56 @@ impl EditorWindow for SceneWindow {
     }
 }
 
-fn save_world(world: &World, name: &str) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+fn save_world(world: &World, name: &str, entitys: std::collections::HashSet<Entity>) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let type_registry = world.get_resource::<TypeRegistry>().unwrap();
-    let scene = DynamicScene::from_world(&world, type_registry);
+    let scene = from_world(&world, type_registry, entitys);
 
     let ron = scene.serialize_ron(type_registry)?;
     std::fs::write(name, ron)?;
 
     Ok(())
+}
+
+/// Create a new dynamic scene from a given world and etity set;
+fn from_world(world: &World, type_registry: &TypeRegistry, entitys: std::collections::HashSet<Entity>) -> DynamicScene {
+    use bevy::scene::DynamicEntity;
+    let mut scene = DynamicScene::default();
+    let type_registry = type_registry.read();
+
+    for archetype in world.archetypes().iter() {
+        // a map of an entity to its index in the dynamic scene;
+        // can't use simple offset like bevy's because it may skip an entity
+        let mut entity_map = std::collections::HashMap::new();
+        // Create a new dynamic entity for each entity of the given archetype
+        // and insert it into the dynamic scene.
+        // skip entitys not in the set
+        for entity in archetype.entities() {
+            if !entitys.contains(entity) {continue;}
+            entity_map.insert(entity, scene.entities.len());
+            scene.entities.push(DynamicEntity {
+                entity: entity.id(),
+                components: Vec::new(),
+            });
+        }
+
+        // Add each reflection-powered component to the entity it belongs to.
+        for component_id in archetype.components() {
+            let reflect_component = world
+                .components()
+                .get_info(component_id)
+                .and_then(|info| type_registry.get(info.type_id().unwrap()))
+                .and_then(|registration| registration.data::<ReflectComponent>());
+            if let Some(reflect_component) = reflect_component {
+                for entity in archetype.entities() {
+                    if !entitys.contains(entity) {continue;}
+                    if let Some(component) = reflect_component.reflect(world, *entity) {
+                        scene.entities[*entity_map.get(entity).expect("entity to have been added to map")]
+                            .components
+                            .push(component.clone_value());
+                    }
+                }
+            }
+        }
+    }
+    scene
 }
