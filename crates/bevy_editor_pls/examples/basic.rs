58997923c24445720a4cbf914aa7e897c8d08104
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-06-09 20:55:38 +1000

    revert: basic example accidentally committed changes

--- crates/bevy_editor_pls/examples/basic.rs
+++ crates/bevy_editor_pls/examples/basic.rs
@@ -47,7 +47,7 @@ fn setup(
         material: materials.add(Color::rgb(0.8, 0.7, 0.6)),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..Default::default()
-    }).insert(transform_gizmo_bevy::GizmoTarget::default());
+    });
     // light
     commands.spawn(PointLightBundle {
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
@@ -61,5 +61,5 @@ fn setup(
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
-    }).insert(transform_gizmo_bevy::GizmoCamera);
+    });
 }
