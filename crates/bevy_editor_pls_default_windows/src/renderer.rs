8ae7170f4a5851bb5459530510053efa3b95e45e
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-08-11 11:38:31 +0200

    remove auto_shrink false

--- crates/bevy_editor_pls_default_windows/src/renderer.rs
+++ crates/bevy_editor_pls_default_windows/src/renderer.rs
@@ -17,44 +17,42 @@ impl EditorWindow for RendererWindow {
         let type_registry = world.resource::<AppTypeRegistry>().clone();
         let type_registry = type_registry.read();
 
-        egui::ScrollArea::vertical()
-            .auto_shrink([false, false])
-            .show(ui, |ui| {
-                let render_device = world.get_resource::<RenderDevice>().unwrap();
+        egui::ScrollArea::vertical().show(ui, |ui| {
+            let render_device = world.get_resource::<RenderDevice>().unwrap();
 
-                let limits = render_device.limits();
-                let features = render_device.features();
+            let limits = render_device.limits();
+            let features = render_device.features();
 
-                ui.heading("Settings");
-                egui::Grid::new("directional_light_shadow_map").show(ui, |ui| {
-                    let mut directional_light_shadow_map = world
-                        .get_resource_mut::<DirectionalLightShadowMap>()
-                        .unwrap();
-                    ui.label("Directional light shadow map size");
-                    let mut size = directional_light_shadow_map.size;
+            ui.heading("Settings");
+            egui::Grid::new("directional_light_shadow_map").show(ui, |ui| {
+                let mut directional_light_shadow_map = world
+                    .get_resource_mut::<DirectionalLightShadowMap>()
+                    .unwrap();
+                ui.label("Directional light shadow map size");
+                let mut size = directional_light_shadow_map.size;
 
-                    let mut context = Context::default();
-                    let mut env = InspectorUi::new_no_short_circuit(&type_registry, &mut context);
-                    if env.ui_for_reflect_with_options(
-                        &mut size,
-                        ui,
-                        egui::Id::NULL,
-                        &NumberOptions::at_least(1).with_speed(4.0),
-                    ) {
-                        directional_light_shadow_map.size = size;
-                    }
-                    ui.end_row();
-                });
-
-                ui.collapsing("Limits", |ui| {
-                    ui.label(RichText::new(format!("{:#?}", limits)).monospace());
-                });
-                ui.collapsing("Features", |ui| {
-                    let features = format!("{:#?}", features);
-                    for feature in features.split(" | ") {
-                        ui.label(RichText::new(format!("- {}", feature)).monospace());
-                    }
-                });
+                let mut context = Context::default();
+                let mut env = InspectorUi::new_no_short_circuit(&type_registry, &mut context);
+                if env.ui_for_reflect_with_options(
+                    &mut size,
+                    ui,
+                    egui::Id::NULL,
+                    &NumberOptions::at_least(1).with_speed(4.0),
+                ) {
+                    directional_light_shadow_map.size = size;
+                }
+                ui.end_row();
             });
+
+            ui.collapsing("Limits", |ui| {
+                ui.label(RichText::new(format!("{:#?}", limits)).monospace());
+            });
+            ui.collapsing("Features", |ui| {
+                let features = format!("{:#?}", features);
+                for feature in features.split(" | ") {
+                    ui.label(RichText::new(format!("- {}", feature)).monospace());
+                }
+            });
+        });
     }
 }
