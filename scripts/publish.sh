56aed0cfc8a96aa5e49252bb9574ba95a73bb791
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-10-14 16:03:16 +0200

    fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-cargo publish -p bevy_editor_pls_core
-cargo publish -p bevy_editor_pls_default_windows
+cargo publish -p bevy_editor_pls_core --features bevy/x11
+cargo publish -p bevy_editor_pls_default_windows --features bevy/x11
 cargo publish -p bevy_editor_pls
